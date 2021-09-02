fe8456a207d57279563dd90b985519b694a66013
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-09-02 15:16:28 -0400

    Reformat TerminatorPlus.java

--- src/main/java/net/nuggetmc/tplus/TerminatorPlus.java
+++ src/main/java/net/nuggetmc/tplus/TerminatorPlus.java
@@ -8,48 +8,44 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.Arrays;
 
 public class TerminatorPlus extends JavaPlugin {
+	private static TerminatorPlus instance;
+	private static String version;
+	private BotManager manager;
+	private CommandHandler handler;
 
-    private static TerminatorPlus instance;
-    private static String version;
+	public static TerminatorPlus getInstance() {
+		return instance;
+	}
 
-    private BotManager manager;
-    private CommandHandler handler;
+	public static String getVersion() {
+		return version;
+	}
 
-    public static TerminatorPlus getInstance() {
-        return instance;
-    }
+	public BotManager getManager() {
+		return manager;
+	}
 
-    public static String getVersion() {
-        return version;
-    }
+	public CommandHandler getHandler() {
+		return handler;
+	}
 
-    public BotManager getManager() {
-        return manager;
-    }
+	@Override
+	public void onEnable() {
+		instance = this;
+		version = getDescription().getVersion();
+//		Create Instances
+		this.manager = new BotManager();
+		this.handler = new CommandHandler(this);
+//		Register event listeners
+		this.registerEvents(manager);
+	}
 
-    public CommandHandler getHandler() {
-        return handler;
-    }
+	@Override
+	public void onDisable() {
+		manager.reset();
+	}
 
-    @Override
-    public void onEnable() {
-        instance = this;
-        version = getDescription().getVersion();
-
-        // Create Instances
-        this.manager = new BotManager();
-        this.handler = new CommandHandler(this);
-
-        // Register event listeners
-        this.registerEvents(manager);
-    }
-
-    @Override
-    public void onDisable() {
-        manager.reset();
-    }
-
-    private void registerEvents(Listener... listeners) {
-        Arrays.stream(listeners).forEach(li -> this.getServer().getPluginManager().registerEvents(li, this));
-    }
+	private void registerEvents(Listener... listeners) {
+		Arrays.stream(listeners).forEach(li -> this.getServer().getPluginManager().registerEvents(li, this));
+	}
 }
