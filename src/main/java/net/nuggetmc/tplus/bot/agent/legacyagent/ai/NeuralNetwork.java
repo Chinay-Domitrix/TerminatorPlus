b157bc1b52a1a162353eb4a716f3bb629ef0cb71
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-09-09 14:15:47 -0400

    Reformat NeuralNetwork.java

--- src/main/java/net/nuggetmc/tplus/bot/agent/legacyagent/ai/NeuralNetwork.java
+++ src/main/java/net/nuggetmc/tplus/bot/agent/legacyagent/ai/NeuralNetwork.java
@@ -8,87 +8,77 @@ import org.apache.commons.lang.StringUtils;
 import java.util.*;
 
 public class NeuralNetwork {
+	// thinking about making an enum called BotNode, and have a map here, .check(Node.L) or fetch
+	// also randomize activation point between 0 and 0.5
+	// randomize the blocking length and cooldown
+	// also the XZ offset randomizers!! (or maybe just turn them off entirely, that works too)
+	// b, j, l, r, ox, oz, av, bl, bc, dlr
+	private final Map<BotNode, NodeConnections> nodes;
+	private final boolean dynamicLR;
+	public static final NeuralNetwork RANDOM = new NeuralNetwork(new HashMap<>());
 
-    // thinking about making an enum called BotNode, and have a map here, .check(Node.L) or fetch
-    // also randomize activation point between 0 and 0.5
-    // randomize the blocking length and cooldown
-    // also the XZ offset randomizers!! (or maybe just turn them off entirely, that works too)
+	private NeuralNetwork(Map<BotNode, Map<BotDataType, Double>> profile) {
+		this.nodes = new HashMap<>();
+		this.dynamicLR = true;
+		if (profile == null) Arrays.stream(BotNode.values()).forEach(n -> this.nodes.put(n, new NodeConnections()));
+		else profile.forEach((nodeType, map) -> nodes.put(nodeType, new NodeConnections(map)));
+		
+	}
 
-    // b, j, l, r, ox, oz, av, bl, bc, dlr
+	public static NeuralNetwork createNetworkFromProfile(Map<BotNode, Map<BotDataType, Double>> profile) {
+		return new NeuralNetwork(profile);
+	}
 
-    private final Map<BotNode, NodeConnections> nodes;
+	public static NeuralNetwork generateRandomNetwork() {
+		return new NeuralNetwork(null);
+	}
 
-    private final boolean dynamicLR;
+	public NodeConnections fetch(BotNode node) {
+		return nodes.get(node);
+	}
 
-    public static final NeuralNetwork RANDOM = new NeuralNetwork(new HashMap<>());
+	public boolean check(BotNode node) {
+		return nodes.get(node).check();
+	}
 
-    private NeuralNetwork(Map<BotNode, Map<BotDataType, Double>> profile) {
-        this.nodes = new HashMap<>();
-        this.dynamicLR = true;
+	public double value(BotNode node) {
+		return nodes.get(node).value();
+	}
 
-        if (profile == null) {
-            Arrays.stream(BotNode.values()).forEach(n -> this.nodes.put(n, new NodeConnections()));
-        } else {
-            profile.forEach((nodeType, map) -> nodes.put(nodeType, new NodeConnections(map)));
-        }
-    }
+	public void feed(BotData data) {
+		nodes.values().forEach(n -> n.test(data));
+	}
 
-    public static NeuralNetwork createNetworkFromProfile(Map<BotNode, Map<BotDataType, Double>> profile) {
-        return new NeuralNetwork(profile);
-    }
+	public Map<BotNode, NodeConnections> nodes() {
+		return nodes;
+	}
 
-    public static NeuralNetwork generateRandomNetwork() {
-        return new NeuralNetwork(null);
-    }
+	public boolean dynamicLR() {
+		return dynamicLR;
+	}
 
-    public NodeConnections fetch(BotNode node) {
-        return nodes.get(node);
-    }
+	public Map<BotNode, Map<BotDataType, Double>> values() {
+		Map<BotNode, Map<BotDataType, Double>> output = new HashMap<>();
+		nodes.forEach((nodeType, node) -> output.put(nodeType, node.getValues()));
+		return output;
+	}
 
-    public boolean check(BotNode node) {
-        return nodes.get(node).check();
-    }
+	public String output() {
+		List<String> strings = new ArrayList<>();
+		nodes.forEach((type, node) -> strings.add(type.name().toLowerCase() + "=" + (node.check() ? (ChatUtils.ON + "1") : (ChatUtils.OFF + "0")) + ChatColor.RESET));
+		Collections.sort(strings);
+		return "[" + StringUtils.join(strings, ", ") + "]";
+	}
 
-    public double value(BotNode node) {
-        return nodes.get(node).value();
-    }
-
-    public void feed(BotData data) {
-        nodes.values().forEach(n -> n.test(data));
-    }
-
-    public Map<BotNode, NodeConnections> nodes() {
-        return nodes;
-    }
-
-    public boolean dynamicLR() {
-        return dynamicLR;
-    }
-
-    public Map<BotNode, Map<BotDataType, Double>> values() {
-        Map<BotNode, Map<BotDataType, Double>> output = new HashMap<>();
-        nodes.forEach((nodeType, node) -> output.put(nodeType, node.getValues()));
-        return output;
-    }
-
-    public String output() {
-        List<String> strings = new ArrayList<>();
-        nodes.forEach((type, node) -> strings.add(type.name().toLowerCase() + "=" + (node.check() ? ChatUtils.ON + "1" : ChatUtils.OFF + "0") + ChatColor.RESET));
-        Collections.sort(strings);
-        return "[" + StringUtils.join(strings, ", ") + "]";
-    }
-
-    @Override
-    public String toString() {
-        List<String> strings = new ArrayList<>();
-
-        nodes.forEach((nodeType, node) -> {
-            List<String> values = new ArrayList<>();
-            values.add("name=\"" + nodeType.name().toLowerCase() + "\"");
-            node.getValues().forEach((dataType, value) -> values.add(dataType.getShorthand() + "=" + MathUtils.round2Dec(value)));
-            strings.add("{" + StringUtils.join(values, ",") + "}");
-        });
-
-        return "NeuralNetwork{nodes:[" + StringUtils.join(strings, ",") + "]}";
-    }
+	@Override
+	public String toString() {
+		List<String> strings = new ArrayList<>();
+		nodes.forEach((nodeType, node) -> {
+			List<String> values = new ArrayList<>();
+			values.add("name=\"" + nodeType.name().toLowerCase() + "\"");
+			node.getValues().forEach((dataType, value) -> values.add(dataType.getShorthand() + "=" + MathUtils.round2Dec(value)));
+			strings.add("{" + StringUtils.join(values, ",") + "}");
+		});
+		return "NeuralNetwork{nodes:[" + StringUtils.join(strings, ",") + "]}";
+	}
 }
